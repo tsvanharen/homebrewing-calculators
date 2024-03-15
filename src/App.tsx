bf91d9085f5a16c8995d7ea4d39b7a540438f265
Tim S. Van Haren <tsvanharen@gmail.com>
2024-03-15 12:06:42 -0400

    fix: remove router basename

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,8 @@ import {
 import { Calculators, Render } from 'types/Calculator';
 
 export default function App() {
-  // part of a hack to get the site to run on GitHub Pages: https://github.com/rafgraph/spa-github-pages
-  const baseName = (process.env.NODE_ENV === 'development' ? '' : '/homebrewing-calculators');
-
   return (
-    <BrowserRouter basename={baseName}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route
